792b0ab3d678e0b5f2650cd9a30f53530bc6d9ed
Lukas <lucasfasilva27@gmail.com>
2022-05-31 11:47:29 -0300

    Update Front end

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -4,6 +4,12 @@ import { Subscription } from 'rxjs';
 
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
+import { HttpResponse } from '@angular/common/http';
+import { IInfluenciador } from 'app/entities/influenciador/influenciador.model';
+import { ITEMS_PER_PAGE } from 'app/config/pagination.constants';
+import { IEmpresa } from 'app/entities/empresa/empresa.model';
+import { EmpresaService } from 'app/entities/empresa/service/empresa.service';
+import { InfluenciadorService } from 'app/entities/influenciador/service/influenciador.service';
 
 @Component({
   selector: 'jhi-home',
@@ -13,11 +19,31 @@ import { Account } from 'app/core/auth/account.model';
 export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
   authSubscription?: Subscription;
+  empresas?: IEmpresa[];
+  influenciadors?: IInfluenciador[];
+  isLoading = false;
+  totalItems = 0;
+  itemsPerPage = ITEMS_PER_PAGE;
+  page?: number;
+  predicate!: string;
+  ascending!: boolean;
+  ngbPaginationPage = 1;
 
-  constructor(private accountService: AccountService, private router: Router) {}
+  constructor(
+    private accountService: AccountService,
+    protected empresaService: EmpresaService,
+    protected influenciadorService: InfluenciadorService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
+    this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => {
+      this.account = account;
+    });
+    // eslint-disable-next-line no-console
+    this.accountService.getAuthenticationState().subscribe(console.log);
+    this.loadEmpresa();
+    this.loadInfluenciador();
   }
 
   isAuthenticated(): boolean {
@@ -32,5 +58,57 @@ export class HomeComponent implements OnInit, OnDestroy {
     if (this.authSubscription) {
       this.authSubscription.unsubscribe();
     }
+  }
+
+  loadEmpresa(page?: number, dontNavigate?: boolean): void {
+    this.isLoading = true;
+    const pageToLoad: number = page ?? this.page ?? 1;
+
+    this.empresaService
+      .query({
+        page: pageToLoad - 1,
+        size: this.itemsPerPage,
+        // sort: this.sort(),
+      })
+      .subscribe(
+        (res: HttpResponse<IEmpresa[]>) => {
+          this.isLoading = false;
+          this.empresas = res.body ?? [];
+        },
+        () => {
+          this.isLoading = false;
+          // this.onError();
+        }
+      );
+  }
+
+  loadInfluenciador(page?: number, dontNavigate?: boolean): void {
+    this.isLoading = true;
+    const pageToLoad: number = page ?? this.page ?? 1;
+
+    this.influenciadorService
+      .query({
+        page: pageToLoad - 1,
+        size: this.itemsPerPage,
+        // sort: this.sort(),
+      })
+      .subscribe(
+        (res: HttpResponse<IInfluenciador[]>) => {
+          this.isLoading = false;
+          this.influenciadors = res.body ?? [];
+        },
+        () => {
+          this.isLoading = false;
+          // this.onError();
+        }
+      );
+  }
+
+  protected sort(): string[] {
+    const result = [this.predicate + ',' + (this.ascending ? 'asc' : 'desc')];
+    if (this.predicate !== 'id') {
+      result.push('id');
+    }
+    return result;
   }
 }
